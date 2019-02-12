0c64197d27a3d59a7ccd351c9fe8138e42c3856f
Shubham1706 <shubham.d.1706@gmail.com>
2019-02-12 23:57:05 +0530

    Added a data structure.

---
+++ Stacks/Stack.py
@@ -0,0 +1,43 @@
+# Defining a class for node or an element with attributes value and next node.
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+# Defining a class for LinkedList so nodes can be manipulated.
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, new_node):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+
+    def insert_first(self, new_node):
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete_first(self):
+        if self.head:
+            deleted_node = self.head
+            temp = deleted_node.next
+            self.head = temp
+            return deleted_node
+        else:
+            return None
+
+# Defining a class for Stack, which performs two operations: push and pop. Stack uses FIFO (First IN First OUT) policy to add or remove elements.
+class Stack(object):
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
+
+    def push(self, new_node):
+        self.ll.insert_first(new_node)
+
+    def pop(self):
+        return self.ll.delete_first()
